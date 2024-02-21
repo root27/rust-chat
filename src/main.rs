51f568a358a4e062ea08800067fd3456fb5a9e98
root27 <oguzhaneee@gmail.com>
2024-02-21 12:35:29 +0300

    basic tcp listerner

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,96 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::{TcpListener, TcpStream};
+
+use std::io::Write;
+
+use std::thread;
+
+use std::sync::mpsc::{channel, Sender, Receiver};
+
+
+
+
+enum Message {
+    Connected,
+    Disconnected,
+    NewMessage,
+}
+
+
+
+
+
+fn server(message: Receiver<Message>){
+
+}
+
+
+
+
+
+
+fn client(mut stream: TcpStream, message: Sender<Message>) -> Result<(),()> {
+
+
+    let _ = writeln!(stream, "Hello from server").map_err(|err| {
+
+        eprintln!("ERROR: Writing message to user {err}")
+
+    });
+
+    Ok(())
+
+
+
+
+}
+
+
+
+
+fn main() -> Result<(),()> {
+
+        let address = "127.0.0.1:3030";
+
+
+        let listener = TcpListener::bind(address).map_err(|error| {
+            eprintln!("Error binding to {address}: {error}");
+        })?;
+
+
+
+        println!("Listening on {address}");
+
+
+        let (message_sender, message_receiver): (Sender<Message>, Receiver<Message>) = channel();
+
+        thread::spawn(|| server(message_receiver)); 
+
+
+        for stream in listener.incoming() {
+
+            match stream {
+
+                Ok(stream) => {
+
+                    let message_sender = message_sender.clone();
+
+                    thread::spawn(|| client(stream, message_sender));
+
+                }
+
+                Err(err) => {
+                    
+                    eprintln!("ERROR: Accepting connections:{err}")
+
+                }
+
+            }
+
+
+        }
+
+    
+
+    Ok(())
+      
 }
