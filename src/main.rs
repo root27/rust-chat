3c323955c1237b31d6dc7b40770e7238f4bda670
root27 <oguzhaneee@gmail.com>
2024-02-21 14:14:36 +0300

    client and server implemented

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,99 @@
+use std::collections::HashMap;
 use std::net::{TcpListener, TcpStream};
 
 use std::io::Write;
+use std::io::Read;
 
 use std::thread;
 
 use std::sync::mpsc::{channel, Sender, Receiver};
 
+use std::sync::Arc;
+
 
 
 
 enum Message {
-    Connected,
-    Disconnected,
-    NewMessage,
+    Connected{client: Arc<TcpStream>},
+    Disconnected{client: Arc<TcpStream>},
+    NewMessage{
+        data: Vec<u8>,
+        client: Arc<TcpStream>,
+    }
+}
+
+
+struct Client {
+    Conn: Arc<TcpStream>,
 }
 
 
 
 
 
-fn server(message: Receiver<Message>){
+
+
+fn server(messages: Receiver<Message>){
+
+    let mut  clients = HashMap::new();
+
+
+    loop {
+
+        let msg = messages.recv().expect("ERROR: Receiving message from client");
+
+        match msg {
+
+
+            Message::Connected{client} => {
+
+                let address = client.peer_addr().unwrap();
+
+             
+                clients.insert(address.clone(), Client{Conn: client.clone()});
+
+                println!("Client connected: {}", client.peer_addr().unwrap());
+
+            }
+
+            Message::Disconnected{client} => {
+
+                let address = client.peer_addr().unwrap();
+
+                clients.remove(&address);
+
+                println!("Client disconnected: {}", client.peer_addr().unwrap());
+
+
+            }
+
+            Message::NewMessage{data, client} => {
+
+                let address = client.peer_addr().unwrap();
+
+                for (client_address, client) in clients.iter() {
+
+                    if client_address != &address {
+
+                        client.Conn.as_ref().write(&data).expect("ERROR: Sending message to client");
+
+                    }
+
+                }
+
+             
+
+            }
+
+
+        }
+
+    }
+
+
+
+  
+
 
 }
 
@@ -28,16 +102,61 @@ fn server(message: Receiver<Message>){
 
 
 
-fn client(mut stream: TcpStream, message: Sender<Message>) -> Result<(),()> {
+fn client(mut stream: Arc<TcpStream>, message: Sender<Message>) -> Result<(),()> {
 
 
-    let _ = writeln!(stream, "Hello from server").map_err(|err| {
+    
+    message.send(Message::Connected{client: stream.clone()}).map_err(|err| {
 
-        eprintln!("ERROR: Writing message to user {err}")
+        eprintln!("ERROR: Sending message to server {err}");
 
-    });
+       let _ =  message.send(Message::Disconnected{client: stream.clone()}).map_err(|err| {
 
-    Ok(())
+            eprintln!("ERROR: Sending message to server {err}");
+
+        });
+
+
+    })?;
+
+
+    
+    let mut buffer = [0; 1024];
+
+    loop {
+
+        let bytes_read = stream.as_ref().read(&mut buffer).map_err(|err| {
+
+            eprintln!("ERROR: Reading from user {err}");
+
+           let _ = message.send(Message::Disconnected{client: stream.clone()}).map_err(|err| {
+
+                eprintln!("ERROR: Sending message to server {err}");
+
+            });
+
+        })?;
+
+        if bytes_read == 0 {
+
+            message.send(Message::Disconnected{client: stream.clone()}).map_err(|err| {
+
+                eprintln!("ERROR: Sending message to server {err}");
+
+            })?;
+
+            return Ok(());
+
+        }
+
+        message.send(Message::NewMessage{data: buffer[0..bytes_read].to_vec(), client: stream.clone()}).map_err(|err| {
+
+            eprintln!("ERROR: Sending message to server {err}");
+
+        })?;
+
+    }
+
 
 
 
@@ -71,6 +190,9 @@ fn main() -> Result<(),()> {
             match stream {
 
                 Ok(stream) => {
+
+
+                    let stream = Arc::new(stream);
 
                     let message_sender = message_sender.clone();
 
